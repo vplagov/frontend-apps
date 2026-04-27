50ea67a2e8779d4169b4384227204863abb9cd9f
Vitali Plagov <vplagov@pm.me>
2026-04-28 00:29:08 +0300

    test: add HttpClientTestingModule to app specs to resolve CI failures

--- projects/rss-feed-ui/src/app/app.spec.ts
+++ projects/rss-feed-ui/src/app/app.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { App } from './app';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, App],
+      imports: [RouterTestingModule, App, HttpClientTestingModule],
     }).compileComponents();
   });
 
